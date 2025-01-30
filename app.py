435dcfc9dbb796a6a31160be19e190cc6a88cfd4
Robert Redburn <rredburn@us.ibm.com>
2025-01-30 01:05:50 -0500

    update run command

--- app.py
+++ app.py
@@ -492,4 +492,4 @@ def update_graph(datePicked, selectedData, selectedLocation):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run(host="0.0.0.0", debug=True, port="8050")
